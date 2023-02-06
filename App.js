dadb9e66b48e7b7bb823eb8bbf71105d7c3027f0
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-02-06 12:27:12 -0500

    ADD: setup

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider>
+      <View style={styles.container}>
+      <Text>uber clone</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
