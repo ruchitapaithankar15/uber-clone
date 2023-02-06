935947537948c399b534b6f6bad7a8408a6b8cc9
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-02-06 17:44:27 -0500

    ADD: setup

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
-    <Provider store={}>
+    <Provider store={store}>
       <View style={styles.container}>
       <Text>uber clone</Text>
       <StatusBar style="auto" />
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -27,9 +27,9 @@ export const {setOrigin, setDestination,
     setTravelTimeInformation} = navSlice.actions
 
 
-export const selectOrigin = (state) = state.nav.origin;
-export const selectDestination = (state) = state.nav.destination;
-export const selectTravelTimeInformation = (state) = state.nav.travelTimeInformation;
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
 
 
 export default navSlice.reducer;
