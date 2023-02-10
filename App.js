43f045a0d3a85660b89b9e51cf8ee74bbf6336fa
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-02-10 01:45:47 -0500

    ADD: navigation

--- App.js
+++ App.js
@@ -4,14 +4,40 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
+      <NavigationContainer>
       <SafeAreaProvider>
-      <HomeScreen/>
+        <Stack.Navigator>
+          <Stack.Screen
+          name='HomeScreen' 
+          component={HomeScreen}
+          options = {{
+            headerShown: false
+          }}
+          />
+          <Stack.Screen
+          name='MapScreen' 
+          component={MapScreen}
+          options = {{
+            headerShown: false
+          }}
+          />
+        </Stack.Navigator>
+        
+      {/* <HomeScreen/> */}
       </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { Component } from 'react'
 import { Text, StyleSheet, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import { Icon } from 'react-native-elements';
@@ -26,14 +27,16 @@ const data = [
 
 
 const NavOptions =()=> {
-
+    const navigation = useNavigation();
     return (
       <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem = {({item})=> (
-        <TouchableOpacity style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+        onPress={()=>navigation.navigate(item.screen)}
+         style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
             <View>
                 <Image
                 style={{width: 120, height: 120, resizeMode: "contain"}}
