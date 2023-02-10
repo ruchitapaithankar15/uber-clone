36adbbf3ccd4649ab9fcfb004ca89081048f4b5d
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-02-09 22:34:07 -0500

    ADD: Homscreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import tw from 'tailwind-react-native-classnames';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+// import { Image } from 'react-native-web';
+
+function HomeScreen() {
+  return (
+    <SafeAreaView style={tw `bg-white h-full`}>
+        <View style={tw `p-5`}>
+            <Image
+            style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain"
+            }}
+            source={{
+                uri: "https://links.papareact.com/gzs"
+            }}
+            />
+        </View>     
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+
+const styles = StyleSheet.create({
+    text: {
+        color: "blue",
+    },
+});
