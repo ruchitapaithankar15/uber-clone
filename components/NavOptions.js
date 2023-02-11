cdb6f639faef33baf77d370a83a5617f1218e813
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-02-11 17:36:21 -0500

    ADD: MapComponent

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,7 +2,9 @@ import { useNavigation } from '@react-navigation/native';
 import React, { Component } from 'react'
 import { Text, StyleSheet, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import { Icon } from 'react-native-elements';
+import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
+import { selectOrigin } from '../slices/navSlice';
 // import { TouchableOpacity } from 'react-native-web';
 // import { FlatList } from 'react-native-web';
 
@@ -28,6 +30,7 @@ const data = [
 
 const NavOptions =()=> {
     const navigation = useNavigation();
+    const origin = useSelector(selectOrigin);
     return (
       <FlatList
       data={data}
@@ -36,8 +39,9 @@ const NavOptions =()=> {
       renderItem = {({item})=> (
         <TouchableOpacity
         onPress={()=>navigation.navigate(item.screen)}
-         style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-            <View>
+         style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+         disabled={!origin}>
+            <View style={tw `${!origin && "opacity-20"}`}>
                 <Image
                 style={{width: 120, height: 120, resizeMode: "contain"}}
                 source={{uri: item.image}}
---
+++ components/NavigateCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const NavigateCard = () => {
+  return (
+    <View>
+      <Text>Yayyy</Text>
+    </View>
+  )
+}
+
+export default NavigateCard
+
+
+const styles = StyleSheet.create({});
