6ef4aeafdb5567468e2fbf6739ce0e3bed9c8ffb
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2023-02-16 17:25:23 -0500

    ADD: menu button

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import MapView from 'react-native-maps';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { useNavigation } from '@react-navigation/native';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const MapScreen =() => {
   const Stack = createStackNavigator();
+  const navigation = useNavigation();
   return (
     <View>
+      <TouchableOpacity onPress={()=> navigation.navigate("HomeScreen")}
+      style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}>
+        <Icon name='menu'/>
+      </TouchableOpacity>
     <View style={tw`h-1/2`}>
       <Map/>
     </View>
